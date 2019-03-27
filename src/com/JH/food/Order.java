a312d1a4338195cc3f88ef08a318a76712f62fc0
cjh3576 <48780830+cjh3576@users.noreply.github.com>
2019-03-27 11:14:17 +0900

    0326

--- src/com/JH/food/Order.java
+++ src/com/JH/food/Order.java
@@ -3,17 +3,17 @@ package com.JH.food;
 import java.util.Scanner;
 
 public class Order {
-	
+
 	public void order(Food[] food) {
 		View view = new View();
 		boolean check = true;
 		Scanner sc = new Scanner(System.in);
 		Food[] nFood = food;
-		
+
 		while(check) {
 			System.out.println("1. 주문 2. 결산");
 			int num = sc.nextInt();
-			
+
 			switch(num) {
 			case 1:
 				for(int i =0; i<nFood.length ; i++ ) {
@@ -27,23 +27,37 @@ public class Order {
 						System.out.println(nFood[i].name + "을 선택하셨습니다.");
 						System.out.println("몇개 주문하시겠습니까 ? ");
 						int total = sc.nextInt();
-						System.out.println(total + "개 주문하셨습니다");
+
 						nFood[i].total += total;
-						
+						if(nFood[i].store<nFood[i].total) {
+							System.out.println("재고가 부족합니다.");
+							nFood[i].total -= total;
+							break;
+						}
+						System.out.println(total + "개 주문하셨습니다");
+
+
+
+					}
+					else {
+						System.out.println("잘못된 입력입니다");
+						break;
 					}
 				}
-	
-				
 
-				
+
+
+
 				break;
 			case 2:
 				System.out.println("결산");
 				view.totalView(nFood);
 				check = !check;
 				break;
-				
-				
+
+			default:
+				System.out.println("잘못된 입력입니다");
+				break;
 			}
 		}
 	}
