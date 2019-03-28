f9923de46f6c882cc899297744db26700506ad5f
cjh3576 <48780830+cjh3576@users.noreply.github.com>
2019-03-28 17:46:40 +0900

    Update View.java

--- src/com/JH/food/View.java
+++ src/com/JH/food/View.java
@@ -18,7 +18,7 @@ public class View {
 		System.out.println("총 결산");
 		for(int i =0; i<tfood.length; i++) {
 			System.out.println(tfood[i].name + " " + "판매 수량 :  " + tfood[i].total );
-			System.out.println("남은 재고" + (tfood[i].store - tfood[i].total) + "개");
+			System.out.println("남은 재고 :  " + (tfood[i].store - tfood[i].total) + "개");
 			System.out.println("=====================================");
 			sum = sum + (tfood[i].total * tfood[i].price);
 			
